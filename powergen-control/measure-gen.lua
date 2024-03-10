d514006cddac2c53fb33e08e7f4bfb85415f334e
unholynuisance <mtataryn555@gmail.com>
2024-03-10 21:25:32 +0200

    powergen-control: cleanup mesure-gen.lua

--- powergen-control/measure-gen.lua
+++ powergen-control/measure-gen.lua
@@ -1,27 +1,18 @@
 local lib = require("lib")
-
-local os = require("os")
-local component = require("component")
-local term = require("term")
 local serialization = require("serialization")
 
 local generators = lib.get_proxies("gt_machine")
 
 local result = table.thread_map(generators, function(key, generator)
-    local active = generator.isMachineActive()
-    ---if not active do
-    ---  generator.setWorkAllowed(true)
-    ---end
+    local isWorkAllowed = generator.isWorkAllowed()
+
     generator.setWorkAllowed(true)
 
     lib.wait_for_stable_efficiency(generator, 20)
-
     local data = lib.get_generator_sensor_information(generator)
-    if not active then
-        generator.setWorkAllowed(false)
-    end
 
-    os.sleep()
+    generator.setWorkAllowed(isWorkAllowed)
+
     data["priority"] = 0
     data["address"] = generator.address
     return key, data
