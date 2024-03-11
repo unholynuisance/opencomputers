38d07b508feaf2a2263b048444807cf76851206d
unholynuisance <mtataryn555@gmail.com>
2024-03-11 15:13:41 +0200

    measure-gen: add collecting of ramp_time

--- powergen-control/measure-gen.lua
+++ powergen-control/measure-gen.lua
@@ -7,15 +7,22 @@ local generators = lib.get_proxies("gt_machine")
 local result = table.parallel_vmap(generators, function(generator)
     local isWorkAllowed = generator.isWorkAllowed()
 
+    local timeout = 20
+
+    generator.setWorkAllowed(false)
+
+    lib.wait_for_stable_efficiency(generator, timeout)
+
     generator.setWorkAllowed(true)
 
-    lib.wait_for_stable_efficiency(generator, 20)
+    local ramp_time = lib.wait_for_stable_efficiency(generator, timeout)
     local data = lib.get_generator_sensor_information(generator)
 
     generator.setWorkAllowed(isWorkAllowed)
 
-    data["priority"] = 0
-    data["address"] = generator.address
+    data.priority = 0
+    data.address = generator.address
+    data.ramp_time = ramp_time
     return data
 end)
 
