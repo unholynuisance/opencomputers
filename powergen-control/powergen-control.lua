0f893a1b7087b87ca1171e907df8746a289e1446
unholynuisance <mtataryn555@gmail.com>
2024-03-31 00:58:10 +0200

    powergen-control: implement basic powerplant control

--- powergen-control/powergen-control.lua
+++ powergen-control/powergen-control.lua
@@ -1,9 +1,9 @@
 local lib = require("nuisance.lib")
+local table = require("nuisance.table")
+
+local event = require("event")
 local term = require("term")
 local serialization = require("serialization")
-
-local generators = lib.get_proxies("gt_machine")
-local batteries = lib.get_proxies("gt_batterybuffer")
 
 local get_grid_info = function(path)
     local file = io.open(path)
@@ -11,19 +11,131 @@ local get_grid_info = function(path)
     return grid_info
 end
 
-local grid_info = get_grid_info("/etc/grid_info")
+local get_generator_to_enable = function(generators, generators_information)
+    generators = table.vfilter(generators, function(v)
+        return not v.isWorkAllowed()
+    end)
 
-while true do
-    os.sleep(1)
+    -- return generator with max priority and max ramp_rate
+    return table.max(generators, function(a, b)
+        local a_info = generators_information[a.address]
+        local b_info = generators_information[b.address]
 
-    local average_input = lib.get_average_input(batteries)
-    local average_output = lib.get_average_output(batteries)
-    local sensor_information = lib.get_sensor_information(batteries)
+        if a_info.priority < b_info.priority then
+            return true
+        end
 
-    term.clear()
+        if a_info.ramp_rate < b_info.ramp_rate then
+            return true
+        end
 
-    print(string.format("Average input: %d", average_input))
-    print(string.format("Average output: %d", average_output))
-    print(serialization.serialize(sensor_information))
-    print(serialization.serialize(grid_info))
+        return false
+    end)
 end
+
+local get_generator_to_disable = function(generators, generators_information)
+    generators = table.vfilter(generators, function(v)
+        return v.isWorkAllowed()
+    end)
+
+    -- return generator with min priority and max ramp_rate
+    return table.min(generators, function(a, b)
+        local a_info = generators_information[a.address]
+        local b_info = generators_information[b.address]
+
+        if a_info.priority < b_info.priority then
+            return true
+        end
+
+        if a_info.ramp_rate > b_info.ramp_rate then
+            return true
+        end
+
+        return false
+    end)
+end
+
+local main = function(config)
+    local grid_information = get_grid_info("/etc/grid_information")
+    grid_information.generators_information = table.vmap(grid_information.generators_information, function(v)
+        v.ramp_rate = v.output / v.ramp_time
+        return v
+    end)
+
+    local generators = lib.get_proxies("gt_machine")
+    local batteries = lib.get_proxies("gt_batterybuffer")
+
+    local should_stop = false
+
+    event.listen("interrupted", function()
+        should_stop = true
+    end)
+
+    while not should_stop do
+        os.sleep(0)
+
+        local generators_information = lib.get_generators_information(generators)
+        local batteries_information = lib.get_batteries_information(batteries)
+
+        local sum_over_field = function(t, field)
+            return table.vsum(table.vmap(t, function(v)
+                return v[field]
+            end))
+        end
+
+        local batteries_input = sum_over_field(batteries_information, "average_input")
+        local batteries_min_input = 0
+        local batteries_output = sum_over_field(batteries_information, "average_output")
+        local batteries_max_output = sum_over_field(batteries_information, "max_output")
+        local batteries_energy = sum_over_field(batteries_information, "energy")
+        local batteries_max_energy = sum_over_field(batteries_information, "max_energy")
+        local batteries_min_energy = 0
+
+        local max_energy_delta = batteries_min_input - batteries_max_output
+        local time_to_empty_at_max_output = lib.ticks_to_seconds( --
+            (batteries_min_energy - batteries_energy) / max_energy_delta
+        )
+
+        local energy_delta = batteries_input - batteries_output
+        local time_to_empty = lib.ticks_to_seconds( --
+            (batteries_min_energy - batteries_energy) / energy_delta
+        )
+        local time_to_full = lib.ticks_to_seconds( --
+            (batteries_max_energy - batteries_energy) / energy_delta
+        )
+
+        term.clear()
+
+        print(string.format("Energy: %f", batteries_energy))
+        print(string.format("Energy delta: %f", energy_delta))
+        print(string.format("Time to full: %f", time_to_full))
+        print(string.format("Time to empty: %f", time_to_empty))
+
+        if 0 <= time_to_empty_at_max_output and time_to_empty_at_max_output < config.min_time_to_empty then
+            local generator = get_generator_to_enable(generators, grid_information.generators_information)
+            if generator ~= nil then
+                local generator_name = generators_information[generator.address].name
+                print(string.format("Stopping generator %s", generator_name))
+                generator.setWorkAllowed(true)
+                lib.wait_for_stable_efficiency(generator, 20)
+            end
+        end
+
+        if 0 <= time_to_full and time_to_full < config.min_time_to_full then
+            local generator = get_generator_to_disable(generators, grid_information.generators_information)
+            if generator ~= nil then
+                local generator_name = generators_information[generator.address].name
+                print(string.format("Stopping generator %s", generator_name))
+                generator.setWorkAllowed(false)
+                lib.wait_for_stable_efficiency(generator, 20)
+            end
+        end
+    end
+end
+
+local config = {
+    min_time_to_empty = 120,
+    min_time_to_full = 10,
+}
+
+main(config)
